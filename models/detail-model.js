7bc5a9359491917e35e6c34663af4f5714d2c41f
mtmenji <mtmenji@gmail.com>
2024-05-11 15:37:45 -0600

    Checkpoint 2

--- models/detail-model.js
+++ models/detail-model.js
@@ -13,11 +13,7 @@ async function getClassifications(){
 async function getDetailByInvId(inv_id) {
     try {
       const data = await pool.query(
-        `SELECT * FROM public.inventory AS i 
-        JOIN public.classification AS c 
-        ON i.classification_id = c.classification_id 
-        WHERE i.classification_id = $1`,
-        [inv_id]
+        `SELECT * FROM public.inventory`
       )
       return data.rows
     } catch (error) {
