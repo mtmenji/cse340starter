0118a8fb3cdcbb9486ee45af9bd32b39551ce3fa
mtmenji <mtmenji@gmail.com>
2024-05-11 20:20:34 -0600

    Another attempt at fixing port code

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.use((err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || "5500";
+const port = process.env.PORT || 5500;
 const host = process.env.HOST || "localhost";
 
 /* ***********************
